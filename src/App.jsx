ffbe2e69e047ec3948551aea3f28739f0740ea58
Ankit <ankit.dayal1992@gmail.com>
2025-04-09 01:11:36 +0530

    Added Static Sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import './App.css'
+import { languages } from './languages'
 
 function App() {
+
+  const languageElements = languages.map(lang => (
+    <span 
+      className='chip' 
+      style={{
+        backgroundColor: lang.backgroundColor,
+        color: lang.color
+      }}
+      key={lang.name}
+    >
+      {lang.name}
+    </span>
+  ))
+
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+      <section className='game-status'>
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className='language-chips'>
+        {languageElements}
+      </section>
+    </main>
   )
 }
 
