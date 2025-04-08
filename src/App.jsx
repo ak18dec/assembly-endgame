442bc65a0959b20c7cd9628a5b34b9c821ffcfbc
Ankit <ankit.dayal1992@gmail.com>
2025-04-09 02:54:09 +0530

    Alignment Issues CSS fixes

--- src/App.jsx
+++ src/App.jsx
@@ -68,15 +68,34 @@ function App() {
     )
   })
 
+  const gameStatusClass = clsx('game-status', {
+    won: isGameWon,
+    lost: isGameLost
+  })
+
   return (
     <main>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
-      <section className='game-status'>
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
+      <section className={gameStatusClass}>
+        { isGameOver ? (
+            isGameWon ? (
+              <>
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+              </>
+            ) : (
+              <>
+                <h2>Game over!</h2>
+                <p>You lose! Better start learning Assembly 😭</p>
+              </>
+            )
+        ) : (
+          null
+        )
+        }
       </section>
       <section className='language-chips'>
         {languageElements}
